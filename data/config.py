c452ec640e334bb6e8b988f5abab0c3cc2826f85
Ulug'bek Xatamjonov <81636710+UlugbekXatamjonov@users.noreply.github.com>
2023-01-07 11:58:42 +0500

    wdw

--- data/config.py
+++ data/config.py
@@ -7,17 +7,9 @@ env.read_env()
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-# IP = env.str("ip")  # Xosting ip manzili
+IP = env.str("ip")  # Xosting ip manzili
 
-# DB_USER = env.str("DB_USER") # foydalanuvchi nomi
-# DB_PASS = env.str("DB_PASS") # baza paroli
-# DB_NAME = env.str("DB_NAME") # baza nomi
-# DB_HOST = env.str("DB_HOST") # host nomi
-
-
-DB_USER='postgres'
-DB_PASS='aR0PNs78DIqFHhiV4IGw'
-DB_NAME='railway'
-DB_HOST='containers-us-west-32.railway.app'
-
-
+DB_USER = env.str("DB_USER") # foydalanuvchi nomi
+DB_PASS = env.str("DB_PASS") # baza paroli
+DB_NAME = env.str("DB_NAME") # baza nomi
+DB_HOST = env.str("DB_HOST") # host nomi
